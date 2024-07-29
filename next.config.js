e595bf8dbf166d55cce2f460f100543734acfc6b
Stefano <stefano.visentin@anyt1me.com>
2024-07-29 11:49:32 +0200

    🐛

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'src/styles')],
-  },
   images: {
     domains: ['utfs.io'],
   },
